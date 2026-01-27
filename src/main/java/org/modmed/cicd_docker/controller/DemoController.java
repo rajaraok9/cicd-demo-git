423ac58df143097f5c19580ab5638311e8c8ac18
rajaraok9 <rkaveliga@gmail.com>
2026-01-27 19:16:50 +0530

    code commit

--- src/main/java/org/modmed/cicd_docker/controller/DemoController.java
+++ src/main/java/org/modmed/cicd_docker/controller/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/")
     public String home() {
-       return "Build #1: Success!";
-        //return "System Status: Error - Version 1.1";
+      // return "Build #1: Success!";
+        return "System Status: Error - Version 1.1";
     }
 }
